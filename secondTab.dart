d4e0fc034b8c28f80d4949d6a0f36b5cfec4c814
TheAnimatrix <avrsaditya@gmail.com>
2020-05-25 17:22:33 +0530

    Create secondTab.dart

---
+++ secondTab.dart
@@ -0,0 +1,16 @@
+
+import 'package:flutter/material.dart';
+
+class PublicCountdownPage extends StatelessWidget {
+
+  PublicCountdownPage({key}):super(key:key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        child:Text("Welcome to the Public countdown list")
+      ),
+    );
+  }
+}
